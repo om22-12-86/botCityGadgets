9dcd4c2892de488ded08d5983347e8422136f590
om <omchik86@mail.ru>
2024-08-05 15:12:12 +0300

    5 moondaySQlite

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 import asyncio
 import os
+import logging
 
 from aiogram import Bot, Dispatcher, types
-from aiogram.enums import ParseMode
-from aiogram.fsm.strategy import FSMStrategy
-
+from aiogram.enums import ParseMode, UpdateType
 from dotenv import find_dotenv, load_dotenv
+
 load_dotenv(find_dotenv())
 
+from database.engine import create_db, drop_db
 from handlers.user_private import user_private_router
 from handlers.user_group import user_group_router
 from handlers.admin_private import admin_router
-
 from common.bot_cmds_list import private
 
-ALLOWED_UPDATES = ['message, edited_message']
+# Установка уровня логирования для отладки
+logging.basicConfig(level=logging.DEBUG)
 
-bot = Bot(token=os.getenv('TOKEN'), parse_mode=ParseMode.HTML)
+# Инициализация бота
+bot = Bot(token=os.getenv('TOKEN'))
 bot.my_admins_list = []
 
 dp = Dispatcher()
@@ -25,11 +27,22 @@ dp.include_router(user_private_router)
 dp.include_router(user_group_router)
 dp.include_router(admin_router)
 
+async def on_startup(bot):
+    run_param = False
+    if run_param:
+        await drop_db()
+    await create_db()
+
+async def on_shutdown(bot):
+    print('Бот завершил работу')
 
 async def main():
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+
+
     await bot.delete_webhook(drop_pending_updates=True)
-    # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
     await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
-    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
+    await dp.start_polling(bot, allowed_updates=[UpdateType.MESSAGE, UpdateType.EDITED_MESSAGE])
 
 asyncio.run(main())
