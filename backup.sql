474f25888d44348f58ad1dec04ab88cf8a253601
om <omchik86@mail.ru>
2025-02-23 22:18:49 +0300

    23.02 22:20

--- backup.sql
+++ backup.sql
@@ -11,8 +11,8 @@ SET standard_conforming_strings = on;
 -- Roles
 --
 
-CREATE ROLE om;
-ALTER ROLE om WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS;
+CREATE ROLE bot;
+ALTER ROLE bot WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS PASSWORD 'SCRAM-SHA-256$4096:010jaUSeTy0CE9E6QvC/fw==$i7WoA6scA4nENytHmjsSPyqxAlWGQUOxhIWYIxFL+LI=:gL1fp/kVKOTE/J6YdM2VKwGC31O9S1OzIgIv3n2lTXc=';
 
 --
 -- User Configurations
@@ -39,8 +39,56 @@ ALTER ROLE om WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPAS
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 16.8 (Homebrew)
--- Dumped by pg_dump version 16.8 (Homebrew)
+-- Dumped from database version 16.8 (Debian 16.8-1.pgdg120+1)
+-- Dumped by pg_dump version 16.8 (Debian 16.8-1.pgdg120+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- Database "bot" dump
+--
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.8 (Debian 16.8-1.pgdg120+1)
+-- Dumped by pg_dump version 16.8 (Debian 16.8-1.pgdg120+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: bot; Type: DATABASE; Schema: -; Owner: bot
+--
+
+CREATE DATABASE bot WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
+
+
+ALTER DATABASE bot OWNER TO bot;
+
+\connect bot
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -67,8 +115,8 @@ SET row_security = off;
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 16.8 (Homebrew)
--- Dumped by pg_dump version 16.8 (Homebrew)
+-- Dumped from database version 16.8 (Debian 16.8-1.pgdg120+1)
+-- Dumped by pg_dump version 16.8 (Debian 16.8-1.pgdg120+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
